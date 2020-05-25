0d2550a51f2b1fe466e07ab4dc99b593fe2f32f7
Sridevi B <sridevi.db@gmail.com>
2020-05-25 20:54:21 +0530

    Update eva4modeltrainer.py

--- EVA4/eva4modeltrainer.py
+++ EVA4/eva4modeltrainer.py
@@ -2,34 +2,59 @@ from tqdm import tqdm_notebook, tnrange
 from eva4modelstats import ModelStats
 import torch.nn.functional as F
 import torch
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+from loss import ssim, msssim
+from utils import saveresults, show
+import gc
+
+
 
 # https://github.com/tqdm/tqdm
 class Train:
-  def __init__(self, model, dataloader, optimizer, stats, scheduler=None, L1lambda = 0):
+  def __init__(self, model, dataloader, optimizer, stats, scheduler=None, criterion1=None, criterion2=None, L1lambda = 0, tb=None):
     self.model = model
     self.dataloader = dataloader
     self.optimizer = optimizer
     self.scheduler = scheduler
     self.stats = stats
     self.L1lambda = L1lambda
+    self.criterion1 = criterion1
+    self.criterion2 = criterion2
+    self.tb = tb
 
   def run(self):
     self.model.train()
-    pbar = tqdm_notebook(self.dataloader)
-    for data, target in pbar:
+    pbar = tqdm_notebook(enumerate(self.dataloader))
+    for batch_idx, data in pbar:
       # get samples
-      data, target = data.to(self.model.device), target.to(self.model.device)
+      bg, fgbg, mask, depth = data['bg'].to(self.model.device), data['fgbg'].to(self.model.device), data['mask'].to(self.model.device), data['depth'].to(self.model.device)
 
       # Init
       self.optimizer.zero_grad()
-      # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
-      # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
-
-      # Predict
-      y_pred = self.model(data)
+      
+      
+      mask_pred, depth_pred = self.model(bg,fgbg)
+      
 
       # Calculate loss
-      loss = F.nll_loss(y_pred, target)
+      if self.criterion1 is not None:
+        loss1 = self.criterion1(mask_pred, mask)
+        
+
+        m_ssim = torch.clamp((1 - msssim(mask_pred, mask, normalize=True)) * 0.5, 0, 1)
+    
+        loss1 = (0.84 * m_ssim) + (0.16 * loss1)
+      if self.criterion2 is not None:
+        loss2 = self.criterion2(depth_pred, depth)
+        
+
+      d_ssim = torch.clamp( 1 - msssim(depth_pred, depth, normalize=True)*0.5, 0, 1)
+      
+    
+      loss2 = (0.84 * d_ssim) + (0.16 * loss2)
+      #print(loss1.item(), loss2.item(), d_ssim.item())
+      loss = 2 * loss1 + loss2
 
       #Implementing L1 regularization
       if self.L1lambda > 0:
@@ -37,15 +62,32 @@ class Train:
         for param in self.model.parameters():
           reg_loss += torch.sum(param.abs())
         loss += self.L1lambda * reg_loss
-
+      
+      n = self.stats.get_batches()
+      if n%500 == 0:
+        self.tb.add_scalar('loss/train', loss.item(), n)
+      
+      if (n+1) % 3000 == 0:
+        grid = torchvision.utils.make_grid(mask_pred.detach().cpu(), nrow=8, normalize=False)
+        self.tb.add_image('imagesmask', grid, n)
+        grid = torchvision.utils.make_grid(depth_pred.detach().cpu(), nrow=8, normalize=False)
+        self.tb.add_image('imagesdepth', grid, n)
+      
+      
+        saveresults(fgbg.detach().cpu(), "./plots/fgbg"+str(n+1)+".jpg", normalize=True)
+        saveresults(mask.detach().cpu(), "./plots/orimask"+str(n+1)+".jpg")
+        saveresults(depth.detach().cpu(), "./plots/oridepth"+str(n+1)+".jpg")
+        saveresults(mask_pred.detach().cpu(), "./plots/predmask"+str(n+1)+".jpg")
+        saveresults(depth_pred.detach().cpu(), "./plots/preddepth"+str(n+1)+".jpg")
+	
+		
 
       # Backpropagation
       loss.backward()
       self.optimizer.step()
 
       # Update pbar-tqdm
-      pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-      correct = pred.eq(target.view_as(pred)).sum().item()
+      
       lr = 0.0
       if self.scheduler:
         lr = self.scheduler.get_last_lr()[0]
@@ -55,90 +97,102 @@ class Train:
       
       #lr =  if self.scheduler else (self.optimizer.lr_scheduler.get_last_lr()[0] if self.optimizer.lr_scheduler else self.optimizer.param_groups[0]['lr'])
       #print('lr for this batch:", lr)
-      self.stats.add_batch_train_stats(loss.item(), correct, len(data), lr)
+      self.stats.add_batch_train_stats(loss.item(), 0, len(data), lr)
       pbar.set_description(self.stats.get_latest_batch_desc())
       if self.scheduler:
         self.scheduler.step()
+      
 
 class Test:
-  def __init__(self, model, dataloader, stats, scheduler=None):
+  def __init__(self, model, dataloader, stats, scheduler=None, criterion1=None, criterion2=None, tb=None):
     self.model = model
     self.dataloader = dataloader
     self.stats = stats
     self.scheduler = scheduler
     self.loss=0.0
+    self.criterion1 = criterion1
+    self.criterion2 = criterion2
+    self.tb = tb
+	
 
   def run(self):
     self.model.eval()
     with torch.no_grad():
-        for data, target in self.dataloader:
-            data, target = data.to(self.model.device), target.to(self.model.device)
-            output = self.model(data)
-            self.loss = F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+        
+        for batch_idx, data in enumerate(self.dataloader):
+            bg, fgbg, mask, depth = data['bg'].to(self.model.device), data['fgbg'].to(self.model.device), data['mask'].to(self.model.device), data['depth'].to(self.model.device)
             
-            # check for Reduce LR on plateau
-            #https://pytorch.org/docs/stable/optim.html#torch.optim.lr_scheduler.ReduceLROnPlateau
-            '''if self.scheduler and isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
-              print("hello yes i am ")
-              self.scheduler.step(loss)'''
+            mask_pred, depth_pred = self.model(bg, fgbg)
+            
+            # Calculate loss
+            if self.criterion1 is not None:
+              loss1 = self.criterion1(mask_pred, mask)
+            m_ssim = torch.clamp((1 - ssim(mask_pred, mask)) * 0.5, 0, 1)
+    
+            loss1 = (0.84 * m_ssim) + (0.16 * loss1)
 
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            if self.criterion2 is not None:
+                loss2 = self.criterion2(depth_pred, depth)
+
+            d_ssim = torch.clamp((1 - ssim(depth_pred, depth)) * 0.5, 0, 1)
+    
+            loss2 = (0.84 * d_ssim) + (0.16 * loss2)
+
+            self.loss = 2 * loss1 + loss2
+
+            if batch_idx == 0:
+              
+              inp = fgbg.detach().cpu()
+              orimp = mask.detach().cpu()
+              mp = mask_pred.detach().cpu()
+              oridp = depth.detach().cpu()
+              dp = depth_pred.detach().cpu()
+              print("First batch in testing fgbg, (mask, predicted mask), (depth, predicted depth)")
+              show(inp[:8,:,:,:], normalize=True)
+              mdinp = torch.cat([orimp[:8,:,:,:], mp[:8,:,:,:], oridp[:8,:,:,:], dp[:8,:,:,:]],dim=0)
+              show(mdinp)
+                       
             
-            correct = pred.eq(target.view_as(pred)).sum().item()
-            self.stats.add_batch_test_stats(self.loss, correct, len(data))
+            self.stats.add_batch_test_stats(self.loss.item(), 0, len(data))
         
         if self.scheduler and isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
               #print("hello yes i am ")
               self.scheduler.step(self.loss)
 
-class Misclass:
-  def __init__(self, model, dataloader, stats):
-    self.model = model
-    self.dataloader = dataloader
-    self.stats = stats
-
-  def run(self):
-    self.model.eval()
-    with torch.no_grad():
-        for data, target in self.dataloader:
-          if len(self.stats.misclassified_images) == 25:
-            return
-          data, target = data.to(self.model.device), target.to(self.model.device)
-          output = self.model(data)
-          loss = F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-          pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-          is_correct = pred.eq(target.view_as(pred))
-          misclassified_inds = (is_correct==0).nonzero()[:,0]
-          for mis_ind in misclassified_inds:
-            if len(self.stats.misclassified_images) == 25:
-               break
-            self.stats.misclassified_images.append({"target": target[mis_ind].cpu().numpy(), "pred": pred[mis_ind][0].cpu().numpy(),"img": data[mis_ind]})
-            
             
 class ModelTrainer:
-  def __init__(self, model, optimizer, train_loader, test_loader, statspath, scheduler=None, batch_scheduler=False, L1lambda = 0):
+  def __init__(self, model, optimizer, train_loader, test_loader, statspath, scheduler=None, batch_scheduler=False, criterion1=None, criterion2=None, L1lambda = 0):
+    self.tb = SummaryWriter()
     self.model = model
+    
+    #x = torch.rand(1,3,128,128)
+    #self.tb.add_graph(self.model, x.to(self.model.device), x.to(self.model.device))
     self.scheduler = scheduler
     self.batch_scheduler = batch_scheduler
     self.optimizer = optimizer
     self.stats = ModelStats(model, statspath)
-    self.train = Train(model, train_loader, optimizer, self.stats, self.scheduler if self.batch_scheduler else None, L1lambda)
-    self.test = Test(model, test_loader, self.stats,self.scheduler)
-    self.misclass = Misclass(model, test_loader, self.stats)
+    self.criterion1 = criterion1
+    self.criterion2 = criterion2
+    self.train = Train(model, train_loader, optimizer, self.stats, self.scheduler if self.batch_scheduler else None, criterion1=criterion1, criterion2=criterion2, L1lambda=L1lambda, tb=self.tb)
+    self.test = Test(model, test_loader, self.stats,self.scheduler, criterion1=criterion1, criterion2=criterion2, tb=self.tb)
+	
+  
 
   def run(self, epochs=10):
     pbar = tqdm_notebook(range(1, epochs+1), desc="Epochs")
     for epoch in pbar:
+      gc.collect()
       self.train.run()
       self.test.run()
       lr = self.optimizer.param_groups[0]['lr']
-      self.stats.next_epoch(lr)
+      self.stats.next_epochmaskdepth(lr)
       pbar.write(self.stats.get_epoch_desc())
       # need to ake it more readable and allow for other schedulers
       if self.scheduler and not self.batch_scheduler and not isinstance(self.scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
         self.scheduler.step()
-        print(self.scheduler.get_lr())
+        print(self.scheduler.get_last_lr())
       pbar.write(f"Learning Rate = {lr:0.6f}")
-    self.misclass.run()
+      self.tb.close()
+
     # save stats for later lookup
-    self.stats.save()
+    #self.stats.save()
