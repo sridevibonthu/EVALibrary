d695ef31855b304b6aa96c15735c391adb5902cd
Sridevi B <sridevi.db@gmail.com>
2020-05-25 20:53:32 +0530

    Update eva4modelstats.py

--- EVA4/eva4modelstats.py
+++ EVA4/eva4modelstats.py
@@ -37,12 +37,32 @@ class ModelStats:
     self.curr_train_acc += acc
     self.train_samples_seen += cnt
     self.batch_lr.append(lr)
+  
+  def get_batches(self):
+    return self.batches
     
 
-  def add_batch_test_stats(self, loss, acc, cnt):
+  def add_batch_test_stats(self, loss, acc=0, cnt=1):
     self.curr_test_loss += loss
     self.curr_test_acc += acc
     self.test_samples_seen += cnt
+	
+
+  def next_epochmaskdepth(self, lr):
+    self.epochs += 1
+    #print(self.curr_test_loss, self.test_samples_seen, self.curr_train_loss, self.train_samples_seen)
+    self.avg_test_loss.append(self.curr_test_loss/self.test_samples_seen)
+    self.avg_train_loss.append(self.curr_train_loss/self.train_samples_seen)
+    self.lr.append(lr)
+    self.curr_train_loss = 0
+    self.curr_test_loss = 0
+    self.train_samples_seen = 0
+    self.test_samples_seen = 0
+
+    if self.epochs == 1 or self.best_test_loss > self.avg_test_loss[-1]:
+      print(f'Validation loss decreased ({self.best_test_loss:.6f} --> {self.avg_test_loss[-1]:.6f}).  Saving model ...')
+      torch.save(self.model.state_dict(), f"{self.path}/{self.model.name}.pt")
+      self.best_test_loss = self.avg_test_loss[-1]
 
   def next_epoch(self, lr):
     self.epochs += 1
@@ -61,7 +81,7 @@ class ModelStats:
 
     if self.epochs == 1 or self.best_test_loss > self.avg_test_loss[-1]:
       print(f'Validation loss decreased ({self.best_test_loss:.6f} --> {self.avg_test_loss[-1]:.6f}).  Saving model ...')
-      torch.save(self.model.state_dict(), f"{self.path}/{self.model.name}.pt")
+      #torch.save(self.model.state_dict(), f"{self.path}/{self.model.name}.pt")
       self.best_test_loss = self.avg_test_loss[-1]
 
   def save(self):
@@ -76,10 +96,10 @@ class ModelStats:
   def get_latest_batch_desc(self):
     if len(self.batch_train_loss)==0:
       return "first batch"
-    return f'Batch={self.batches} Loss={self.batch_train_loss[-1]:0.4f} Acc={100*self.curr_train_acc/self.train_samples_seen:0.2f}%'
+    return f'Batch={self.batches} Loss={self.batch_train_loss[-1]:0.4f} LR={self.batch_lr[-1]:0.6f}'
   
   def get_misclassified_images(self):
     return self.misclassified_images
   
   def get_epoch_desc(self):
-    return f'Epoch: {self.epochs}, Train set: Average loss: {self.avg_train_loss[-1]:.4f}, Accuracy: {100*self.train_acc[-1]:.2f}%; Test set: Average loss: {self.avg_test_loss[-1]:.4f}, Accuracy: {100*self.test_acc[-1]:.2f}%'
+    return f'Epoch: {self.epochs}, Train set: Average loss: {self.avg_train_loss[-1]:.4f} ; Test set: Average loss: {self.avg_test_loss[-1]:.4f}'
